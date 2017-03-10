6c8692062b9fec9949d385c17f05a555cec5ad01
Alexei Khilchuk <khilchuk.alexei@gmail.com>
2017-03-10 23:41:04 +0300

    disabled caching for pages

--- src/main/java/by/a1qa/filter/AuthFilter.java
+++ src/main/java/by/a1qa/filter/AuthFilter.java
@@ -8,11 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
-
-import static by.a1qa.helpers.CommonData.AQA_JIRA_CLIENT_SESSION_ATTR;
 
 import static by.a1qa.constants.PagesMap.PAGES_MAP;
+import static by.a1qa.helpers.CommonData.AQA_JIRA_CLIENT_SESSION_ATTR;
 
 /**
  * Created by tbegu_000 on 09.02.2017.
@@ -42,6 +40,13 @@ public class AuthFilter implements Filter {
                     " | Session last accessed time: " +
                     new Date(((HttpServletRequest) request).getSession().getLastAccessedTime()));
             String uri = ((HttpServletRequest) request).getRequestURI();
+
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.setHeader("Expires", "Tue, 03 Jul 2001 06:00:00 GMT");
+            resp.setDateHeader("Last-Modified", new Date().getTime());
+            resp.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+            resp.setHeader("Pragma", "no-cache");
+
             if (PAGES_MAP.containsKey(uri)) {
                 verifyAccess(request, response, chain, uri, PAGES_MAP.get(uri));
             }
